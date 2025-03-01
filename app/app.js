a7bb8ee03d85af41edfbed508f03fd7568732533
maraed1032 <0aionkim@gmail.com>
2025-03-01 22:34:55 +0900

    프런트 JacaScript 파일

--- app/app.js
+++ app/app.js
@@ -7,7 +7,10 @@ const home = require("./src/routes/home")
 
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+console.log(express.static(`${__dirname}/src/public`))
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use("/", home); //use -> 미들웨어를 등록해주는 매서드
+
 
 module.exports = app;
