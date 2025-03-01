00b0c16d436679769abb5bf6ef46a1245126b092
maraed1032 <0aionkim@gmail.com>
2025-03-02 00:26:35 +0900

    로그인 요청을 위한 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,21 @@
 "use strcit"
 
-console.log("HELLo");
-console.log("buy")
+const id = document.querySelector('#id');
+const psword = document.querySelector("#psword");
+const loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login)
+
+function login() {
+    const req = {
+        id: id.value,
+    };
+
+    fetch("/login", {
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req)
+    })
+}
