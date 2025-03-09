963151dc8f251ffe0425a3e4ddab276fdbced275
maraed1032 <0aionkim@gmail.com>
2025-03-09 15:19:24 +0900

    회원가입 페이지 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -7,10 +7,12 @@ const output = {
     home: (req, res) => {
         res.render("home/index");
     },
-
     login: (req, res) => {
         res.render("home/login");
     },
+    register: (req, res) => {
+        res.render("home/register");
+    }
 };
 
 const process = {
